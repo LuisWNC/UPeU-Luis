ce2cac47c1a6f28280acfa20613f31e1d3550c42
Alumnos <Alumnos@LC01-PC20>
2016-01-13 12:28:01 -0500

    agregando clase persona y .objeto Luis . proyecto

---
+++ Luis 1/src/pe/edu/upeu/poo/clase1/Main.java
@@ -0,0 +1,38 @@
+package pe.edu.upeu.poo.clase1;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Persona luis = new Persona();
+		luis.nombre = "Luis Wilson";
+		luis.altura = 1.62;
+		luis.apePaterno ="Ninacondo";
+		luis.apeMaterno = "Condori";
+		luis.celular = "No Tengo";
+		luis.direccion = "jr. 1ro de marzo con el jr. huancane";
+		luis.dni = "46785529";
+		luis.email = "matrix_nc16@hotmail.com";
+		luis.estadoCivil = "Soltero";
+		luis.religion = "No Tengo";
+		luis.sexo = "Maculino";
+		luis.tipoSangre = "O+";
+		luis.vive = true;
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+		try {
+			luis.fechaNacimiento = sdf.parse("16/09/1956");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		
+		System.out.println(luis);
+		
+		
+	}
+
+}
